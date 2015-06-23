c9b70d33b8a78069447f2787aaa71e363626121a
wuxianlin <wuxianlinwxl@gmail.com>
2015-06-23 16:12:05 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+oort miui telephony
+==================
+
+工具说明：
+------------------
+1.本工具用于miui6 patchrom适配机型移植小米手机的通信层
+
+2.移植后，可以修复一些bug
+
+使用方法：
+----------
+1.将底包的framework-res.apk放到stockrom/system/framework下
+
+2.将小米手机的对应的apk和jar放到xiaomi/system文件夹的对应位置
+
+3.使用脚本开始移植
+
+     ./port.sh
+
+4.运行完成后，临时目录下的overlay文件夹内的smali用于覆盖对应jar中的smali，临时目录下apk用于直接替换对应的apk
+
+注意事项：
+-------
+1.overlay中的smali可能编译报错，需要将const/high16修改成const
+
+感谢：
+-------
+感谢smali开源项目
+感谢github上众多朋友的开源代码
--- port.sh
+++ port.sh
@@ -25,8 +25,10 @@ $NAME_TO_ID_TOOL $3 $1
 
 rm -rf port_tmp
 mkdir port_tmp
+
 $APKTOOL if -t xiaomi $XIAOMI_SYSTEM/framework/framework-res.apk
 $APKTOOL d -t xiaomi $XIAOMI_SYSTEM/framework/framework-res.apk -o port_tmp/framework-res_xiaomi
+
 $APKTOOL if -t stock $STOCK_SYSTEM/framework/framework-res.apk
 $APKTOOL d -t stock $STOCK_SYSTEM/framework/framework-res.apk -o  port_tmp/framework-res_stock
 
@@ -55,9 +57,11 @@ cp -r port_tmp/telephony-common.jar.out/smali/* port_tmp/overlay/telephony-commo
 $APKTOOL d -t xiaomi $XIAOMI_SYSTEM/app/Stk.apk -o port_tmp/Stk
 modify_id port_tmp/Stk/smali port_tmp/framework-res_xiaomi/res/values/public.xml port_tmp/framework-res_stock/res/values/public.xml
 $APKTOOL b -t stock -a $PORT_TOOLS/aapt -t stock port_tmp/Stk -o port_tmp/Stk.apk
+
 $APKTOOL d -t xiaomi $XIAOMI_SYSTEM/app/TelephonyProvider.apk -o port_tmp/TelephonyProvider
 modify_id port_tmp/TelephonyProvider/smali port_tmp/framework-res_xiaomi/res/values/public.xml port_tmp/framework-res_stock/res/values/public.xml
 $APKTOOL b -t stock -a $PORT_TOOLS/aapt -t stock port_tmp/TelephonyProvider -o port_tmp/TelephonyProvider.apk
+
 $APKTOOL d -t xiaomi $XIAOMI_SYSTEM/priv-app/TeleService.apk -o port_tmp/TeleService
 modify_id port_tmp/TeleService/smali port_tmp/framework-res_xiaomi/res/values/public.xml port_tmp/framework-res_stock/res/values/public.xml
 #modify TeleService by wuxianlin start
